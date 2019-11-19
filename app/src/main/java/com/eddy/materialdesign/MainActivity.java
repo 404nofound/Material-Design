9284787418f59d2b2beb0277dd5ea7247b0193ee
Eddy <eddynjupt@qq.com>
2019-11-18 23:15:22 -0500

    CardView Improve + TabLayout

--- app/src/main/java/com/eddy/materialdesign/MainActivity.java
+++ app/src/main/java/com/eddy/materialdesign/MainActivity.java
@@ -2,7 +2,6 @@ package com.eddy.materialdesign;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,10 +10,10 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,14 +27,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -47,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
                 .setDrawerLayout(drawer)
                 .build();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        //NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
--- app/src/main/java/com/eddy/materialdesign/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/eddy/materialdesign/ui/slideshow/SlideshowFragment.java
@@ -1,23 +1,46 @@
 package com.eddy.materialdesign.ui.slideshow;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.customview.widget.ViewDragHelper;
 import androidx.fragment.app.Fragment;
 
 import com.eddy.materialdesign.R;
+import com.google.android.material.card.MaterialCardView;
 
-public class SlideshowFragment extends Fragment {
+public class SlideshowFragment extends Fragment { //implements
+        //View.OnTouchListener {
+        //ViewDragHelper.Callback {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private ViewDragHelper mTopViewDragHelper;
 
     private SlideshowViewModel slideshowViewModel;
+    private MaterialCardView cardView;
+
+    public static SlideshowFragment newInstance(int index) {
+        SlideshowFragment fragment = new SlideshowFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         //slideshowViewModel = ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+        cardView = root.findViewById(R.id.card_view);
+
+        mTopViewDragHelper = ViewDragHelper.create(container, 1.0f, new ViewDragHelperCallback());
+        //mTopViewDragHelper.
+        //mTopViewDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_TOP);
         /*final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -27,4 +50,55 @@ public class SlideshowFragment extends Fragment {
         });*/
         return root;
     }
+
+    /*@Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ObjectAnimator upAnim = ObjectAnimator.ofFloat(view, "translationZ", 16);
+                upAnim.setDuration(150);
+                upAnim.setInterpolator(new DecelerateInterpolator());
+                upAnim.start();
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                ObjectAnimator downAnim = ObjectAnimator.ofFloat(view, "translationZ", 0);
+                downAnim.setDuration(150);
+                downAnim.setInterpolator(new AccelerateInterpolator());
+                downAnim.start();
+                break;
+        }
+        return false;
+    }*/
+
+
+
+    public class ViewDragHelperCallback extends ViewDragHelper.Callback {
+
+        @Override
+        public boolean tryCaptureView(View v, int i) {
+            Log.d("TTT", "work");
+            return true;
+        }
+
+        @Override
+        public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
+            Log.d("TTT", "work1");
+            if (capturedChild instanceof MaterialCardView) {
+                Log.d("TTT", "work2");
+                ((MaterialCardView)cardView).setDragged(true);
+            }
+        }
+
+        @Override
+        public void onViewReleased(@NonNull View releaseChild, float xVel, float yVel) {
+            Log.d("TTT", "work3");
+            if (releaseChild instanceof MaterialCardView) {
+                Log.d("TTT", "work4");
+                ((MaterialCardView)cardView).setDragged(false);
+            }
+        }
+    }
 }
+
