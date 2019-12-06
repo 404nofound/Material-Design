04d5eb132d50ded1c30e58f088443905e625c1ce
Eddy <eddynjupt@qq.com>
2019-12-06 16:32:11 -0500

    Improve CardView

--- app/src/main/java/com/eddy/materialdesign/ui/cardviews/CardViewFragment.java
+++ app/src/main/java/com/eddy/materialdesign/ui/cardviews/CardViewFragment.java
@@ -1,30 +1,24 @@
 package com.eddy.materialdesign.ui.cardviews;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import androidx.annotation.NonNull;
-import androidx.customview.widget.ViewDragHelper;
 import androidx.fragment.app.Fragment;
 
 import com.eddy.materialdesign.R;
 
-import com.google.android.material.card.MaterialCardView;
-
-public class CardViewFragment extends Fragment { //implements
-        //View.OnTouchListener {
-        //ViewDragHelper.Callback {
+public class CardViewFragment extends Fragment implements View.OnClickListener { //implements
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-    private ViewDragHelper mTopViewDragHelper;
 
-    //private SlideshowViewModel slideshowViewModel;
-    private MaterialCardView cardView;
-    private RatingBar ratingBar;
+    private ImageView favour_card2, favour_card3, bookmark_card3;
+    private boolean favour2Clicked, favour3Clicked, bookmark3Clicked;
 
     public static CardViewFragment newInstance(int index) {
         CardViewFragment fragment = new CardViewFragment();
@@ -36,80 +30,68 @@ public class CardViewFragment extends Fragment { //implements
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        //slideshowViewModel = ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_card_view, container, false);
 
-        cardView = root.findViewById(R.id.card_view);
+        View root = inflater.inflate(R.layout.viewpager_fragment_cardviews, container, false);
 
-        mTopViewDragHelper = ViewDragHelper.create(container, 1.0f, new ViewDragHelperCallback());
-        //mTopViewDragHelper.
-        //mTopViewDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_TOP);
-        /*final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+        favour_card2 = root.findViewById(R.id.img_favorite_cardview_2);
+        favour_card3 = root.findViewById(R.id.img_favorite_cardview_3);
+        bookmark_card3 = root.findViewById(R.id.img_bookmark_cardview_3);
 
-        ratingBar = root.findViewById(R.id.ratingbar_cardview_3);
+        favour_card2.setOnClickListener(this);
+        favour_card3.setOnClickListener(this);
+        bookmark_card3.setOnClickListener(this);
+
+        RatingBar ratingBar = root.findViewById(R.id.ratingbar_cardview_4);
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-
+                ratingBar.setRating(v);
             }
         });
 
         return root;
     }
 
-    /*@Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                ObjectAnimator upAnim = ObjectAnimator.ofFloat(view, "translationZ", 16);
-                upAnim.setDuration(150);
-                upAnim.setInterpolator(new DecelerateInterpolator());
-                upAnim.start();
+    @Override
+    public void onClick(View view) {
+
+        AlphaAnimation alphaAnimationShowIcon = new AlphaAnimation(0.2f, 1.0f);
+        alphaAnimationShowIcon.setDuration(500);
+
+        switch (view.getId()) {
+            case R.id.img_favorite_cardview_2:
+                if (!favour2Clicked) {
+                    favour_card2.setImageResource(R.drawable.ic_favorite_black_24dp);
+                    favour_card2.startAnimation(alphaAnimationShowIcon);
+                    favour2Clicked = true;
+                } else {
+                    favour_card2.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                    favour_card2.startAnimation(alphaAnimationShowIcon);
+                    favour2Clicked = false;
+                }
                 break;
-
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                ObjectAnimator downAnim = ObjectAnimator.ofFloat(view, "translationZ", 0);
-                downAnim.setDuration(150);
-                downAnim.setInterpolator(new AccelerateInterpolator());
-                downAnim.start();
+            case R.id.img_favorite_cardview_3:
+                if (!favour3Clicked) {
+                    favour_card3.setImageResource(R.drawable.ic_favorite_black_24dp);
+                    favour_card3.startAnimation(alphaAnimationShowIcon);
+                    favour3Clicked = true;
+                } else {
+                    favour_card3.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                    favour_card3.startAnimation(alphaAnimationShowIcon);
+                    favour3Clicked = false;
+                }
                 break;
-        }
-        return false;
-    }*/
-
-
-
-    public class ViewDragHelperCallback extends ViewDragHelper.Callback {
-
-        @Override
-        public boolean tryCaptureView(View v, int i) {
-            Log.d("TTT", "work");
-            return true;
-        }
-
-        @Override
-        public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
-            Log.d("TTT", "work1");
-            if (capturedChild instanceof MaterialCardView) {
-                Log.d("TTT", "work2");
-                ((MaterialCardView)cardView).setDragged(true);
-            }
-        }
-
-        @Override
-        public void onViewReleased(@NonNull View releaseChild, float xVel, float yVel) {
-            Log.d("TTT", "work3");
-            if (releaseChild instanceof MaterialCardView) {
-                Log.d("TTT", "work4");
-                ((MaterialCardView)cardView).setDragged(false);
-            }
+            case R.id.img_bookmark_cardview_3:
+                if (!bookmark3Clicked) {
+                    bookmark_card3.setImageResource(R.drawable.ic_bookmark_black_24dp);
+                    bookmark_card3.startAnimation(alphaAnimationShowIcon);
+                    bookmark3Clicked = true;
+                } else {
+                    bookmark_card3.setImageResource(R.drawable.ic_bookmark_border_black_24dp);
+                    bookmark_card3.startAnimation(alphaAnimationShowIcon);
+                    bookmark3Clicked = false;
+                }
+                break;
         }
     }
 }
